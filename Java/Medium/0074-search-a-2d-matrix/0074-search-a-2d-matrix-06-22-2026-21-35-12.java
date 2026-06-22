2428709b319b8a5bfcce1f8fcc97e956fc2488a0
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2026-06-22 21:35:15 +0530

    Array

---
+++ Java/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix-06-22-2026-21-35-12.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       int m=matrix[0].length;
+       int n=matrix.length;
+        int i=0,j=m-1;
+        while(i<n && j>=0){
+            if(target == matrix[i][j]){
+                return true;
+            }
+            if(target > matrix[i][j]){
+                i+=1;
+            }
+            else{
+                j-=1;
+            }
+        }
+        return false;
+    }
+}
