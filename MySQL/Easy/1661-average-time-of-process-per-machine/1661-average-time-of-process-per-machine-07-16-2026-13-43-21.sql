ef8bf5f7e6eec0e7b68df37c1ea8620f5902ba3b
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2026-07-16 13:43:22 +0530

    Database

---
+++ MySQL/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine-07-16-2026-13-43-21.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select a.machine_id , round(avg( a1.timestamp - a.timestamp),3) as processing_time
+from Activity a join Activity a1 
+where a.machine_id=a1.machine_id 
+and a.process_id=a1.process_id
+and  a.activity_type= 'start' 
+and a1.activity_type='end'
+group by a.machine_id ;
