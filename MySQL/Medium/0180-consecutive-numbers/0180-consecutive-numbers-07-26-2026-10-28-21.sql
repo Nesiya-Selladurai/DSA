604598a291ed6a1ac77b6ba4b499577415a36326
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2026-07-26 10:28:21 +0530

    Database

---
+++ MySQL/Medium/0180-consecutive-numbers/0180-consecutive-numbers-07-26-2026-10-28-21.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select distinct num as ConsecutiveNums from (
+    select num, 
+    lag(num) over (order by id) as prev,
+    lead(num) over (order by id) as next
+    from Logs
+) t
+where num=prev and num=next
+;
