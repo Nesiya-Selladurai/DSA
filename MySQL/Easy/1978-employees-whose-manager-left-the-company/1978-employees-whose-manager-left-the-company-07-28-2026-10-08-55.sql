bb53e2f025bbb6d4fd3d55e7ed646d885e1060fc
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2026-07-28 10:08:57 +0530

    Database

---
+++ MySQL/Easy/1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company-07-28-2026-10-08-55.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select employee_id from ( 
+    select employee_id,manager_id from Employees where salary<30000
+) as t where manager_id not in (select employee_id from Employees ) order by employee_id;
