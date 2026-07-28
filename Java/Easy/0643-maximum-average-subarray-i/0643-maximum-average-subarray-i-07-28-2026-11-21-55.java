8b7aea470d024d915db196f693c22311e4e6212d
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2026-07-28 11:21:56 +0530

    Array

---
+++ Java/Easy/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i-07-28-2026-11-21-55.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double findMaxAverage(int[] arr, int k) {
+       int n=arr.length;
+       int sum=0;
+       for(int i=0;i<k;i++){
+        sum+=arr[i];
+       } 
+       int max=sum;
+       for(int i=k;i<n;i++){
+        sum=sum-arr[i-k]+arr[i];
+        max=Math.max(max,sum);
+       }
+       return (double)max/k;
+    }
+}
