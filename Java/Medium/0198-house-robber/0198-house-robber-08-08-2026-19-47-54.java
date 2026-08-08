17214a7c1dc4d58d8410fe7f685ee5c1589c055b
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2026-08-08 19:47:55 +0530

    Array

---
+++ Java/Medium/0198-house-robber/0198-house-robber-08-08-2026-19-47-54.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int solve(int i,int[] nums,int[] dp){
+        if(i>=nums.length){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        dp[i]=Math.max((nums[i]+solve(i+2,nums,dp)) , solve(i+1,nums,dp));
+        return dp[i];
+    }
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
+        return solve(0,nums,dp);
+    }
+}
