2dbb9a36a46597deb515c85ae7a4155d8bd6b4f1
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2026-09-17 16:27:11 +0530

    Array

---
+++ Java/Medium/0713-subarray-product-less-than-k/0713-subarray-product-less-than-k-09-17-2026-16-27-09.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+       int n=nums.length;
+       int l=0,r=0,cnt=0;
+       int pro=1;
+       while(r<n){
+        pro*=nums[r];
+        while(l<n && pro>=k){
+            pro/=nums[l];
+            l++;
+        }
+        cnt+=r-l+1;
+        r++;
+       } 
+       return pro==0 ? 0 : cnt;
+    }
+}
