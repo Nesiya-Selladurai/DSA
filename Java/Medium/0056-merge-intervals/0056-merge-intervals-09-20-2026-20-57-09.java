904938632f388303cf5a51fcc1fe0a06969e730b
Nesiya-Selladurai <nesiyakavitha213@gmail.com>
2026-09-20 20:57:12 +0530

    Array

---
+++ Java/Medium/0056-merge-intervals/0056-merge-intervals-09-20-2026-20-57-09.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int n=intervals.length;
+         int[][] ans=new int[n][2];
+        ans[0]=intervals[0];
+        int j=0;
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=ans[j][1]){
+                ans[j][1]=Math.max(intervals[i][1],ans[j][1]);
+            }else{
+                j++;
+                ans[j]=intervals[i];
+            }
+        }
+        return Arrays.copyOf(ans, j + 1);
+
+    }
+}
